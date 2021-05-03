f3b170fb26a46ddf32333eab2d6615504e3d4299
Anjelica <ambrosio.anjelica@gmail.com>
2021-05-03 11:08:52 -0700

    users can move blocks down

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -3,11 +3,14 @@ import {
   PAUSE, RESUME, RESTART, GAME_OVER
 } from '../actions'
 
-import { defaultState } from '../utils'
-
-import {
+  import {
+  defaultState,
   nextRotation,
-  canMoveTo } from '../utils'
+  canMoveTo,
+  addBlockToGrid,
+  checkRows,
+  randomShape
+} from '../utils'
 
 const gameReducer = (state = defaultState(), action) => {
   const { shape, grid, x, y, rotation, nextShape, score, isRunning } = state
@@ -18,7 +21,7 @@ const gameReducer = (state = defaultState(), action) => {
           return { ...state, rotation: newRotation }
       }
       return state
-      
+
     case MOVE_RIGHT:
     if (canMoveTo(shape, grid, x + 1, y, rotation)) {
       return { ...state, x: x + 1 }
@@ -33,8 +36,31 @@ const gameReducer = (state = defaultState(), action) => {
     return state
 
     case MOVE_DOWN:
-
-      return state
+    // Get the next potential Y position
+    const maybeY = y + 1
+    // Check if the current block can move here
+    if (canMoveTo(shape, grid, x, maybeY, rotation)) {
+        // If so move the block
+        return { ...state, y: maybeY }
+    }
+    // If not place the block
+    const newGrid = addBlockToGrid(shape, grid, x, y, rotation)
+    // reset some things to start a new shape/block
+    const newState = defaultState()
+    newState.grid = newGrid
+    newState.shape = nextShape
+    newState.nextShape = randomShape()
+    newState.score = score
+    newState.isRunning = isRunning
+    if (!canMoveTo(nextShape, newGrid, 0, 4, 0)) {
+        // Game Over
+        console.log("Game Should be over...")
+        newState.shape = 0
+        return { ...state, gameOver: true }
+    }
+    // Update the score based on if rows were completed or not
+    newState.score = score + checkRows(newGrid)
+    return newState
 
     case RESUME:
 
