19b00fb6785694fed583ce0322e5432d10c49bf7
Anjelica <ambrosio.anjelica@gmail.com>
2021-05-03 13:18:09 -0700

    project complete

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -2,50 +2,61 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { moveDown, moveLeft, moveRight, rotate } from "../actions";
 
+
 export default function Controls(props) {
   const dispatch = useDispatch();
-  const isRunning = useSelector((state) => state.isRunning);
+  const { isRunning, gameOver } = useSelector((state) => state.game);
   return (
-    <div className="controls">
+    <div className={`controls`}>
+
       {/* left */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveLeft());
         }}
-      >
-        Left
-      </button>
+      >Left</button>
 
       {/* right */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveRight());
         }}
-      >
-        Right
-      </button>
+      >Right</button>
 
       {/* rotate */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(rotate());
         }}
-      >
-        Rotate
-      </button>
+      >Rotate</button>
 
       {/* down */}
       <button
+        disabled={!isRunning || gameOver}
         className="control-button"
         onClick={(e) => {
+          if (!isRunning || gameOver) {
+            return;
+          }
           dispatch(moveDown());
         }}
-      >
-        Down
-      </button>
+      >Down</button>
+
     </div>
   );
 }
